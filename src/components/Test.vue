c66a3ed391c4a01b01415a9869f1420ed4f150d8
avicndugu <avicnduguh@gmail.com>
2019-01-02 22:19:07 +0300

    Made a hello world app

---
+++ src/components/Test.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="test">
+		<h1>{{title}}</h1>
+		<h2>{{user.firstName}}</h2>
+		<h2 v-text="user.lastName"></h2>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'test',
+		data () {
+			return {
+				title:'Hello World',
+				user:{
+					firstName:'Janet',
+					lastName:'Kega'
+				}
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+/style>
